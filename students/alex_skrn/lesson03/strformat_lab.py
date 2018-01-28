79f8d561ad41833b6d5a3bbd4d20355cc8e055ac
AlexSkrn <askrn123@gmail.com>
2018-01-28 23:14:39 +0300

    Modified some code

--- students/alex_skrn/lesson03/strformat_lab.py
+++ students/alex_skrn/lesson03/strformat_lab.py
@@ -2,36 +2,49 @@
 
 """Lesson03 - Assignment 2 - String formatting."""
 
-# Task 1
-def get_padding_zeros(num_zeros):
-    """Ruturn  a str containg num_zeros of padding zeros."""
-    return ''.join(['0'] * num_zeros)
 
+# Task 1
 def get_file_num(an_integer):
     """Return a string like 'file_002' from an_integer."""
-    length = len(str(an_integer))
-    num_zeros = 3 - length
-    padding_zeros = get_padding_zeros(num_zeros)
-    file_num = 'file_' + padding_zeros + str(an_integer)
-    return file_num
+    return "{}{:03d}".format('file_', an_integer)
 
 
 def get_rounded_float_string(a_float):
     """Return a str containing a float rounded to 2 digits afer  decimal."""
-    rounded_float_s = str(round(a_float[1], 2))
-    return rounded_float_s
+    return '{:.2f}'.format(a_float)
 
 
-def convert_int_to_sci_not(an_int):
-    """Return a str with an_int in sci notation, 2 decimal places shown."""
+def convert_int_to_sci_not(a_num):
+    """Return a str with a_num in sci notation, 2 decimal places shown."""
     # Eg. 10000 --> 1.00e+04; 2 --> 2.00e+00; -300 --> 3.00e-02
-    absol_val = abs(an_int)
-
-    if an_int > 0:
-
-    elif an_int < 0:
-
-    elif an_int == 0:
+    # Get the index of  the decimal point
+    # num_str = str(float(a_num))
+    # decimal_pos = num_str.index('.')
+    # if a_num < 0:
+    #     coeff =
+    # else:
+    #     sign = ''
+    #
+    #
+    # if abs(a_num) > 0 and abs(a_num) < 1:
+    #     power_sign = '-'
+    #     power = 0
+    #     for i in str(abs(a_num))[2:]:
+    #         if i == 0:
+    #             power += 1
+    #         else:
+    #             power += 1
+    #             break
+    #     coefficient = float(str(abs(a_num))[power + 1:])
+    # else:
+    #     power_sign = '+'
+    #     power = len(str(abs(a_num))) - 1
+    #     coefficient = abs(a_num) / (10**power)
+    # if a_num < 0:
+    #     sign = '-'
+    # else:
+    #     sign = ''
+    # return "{}{:.02f}e{}{:02d}".format(sign, coefficient, power_sign, power)
 
 
 def convert_float_to_sci_not(a_float):
@@ -133,14 +146,11 @@ def test_task_one_style_formatter():
     string = 'file_002 :   123.46, 1.00e+04, 1.23e+04'
     assert task_one_style_formatter((2, 123.4567, 10000, 12345.67)) == string
 
-    print("Passed test_task_one_style_formatter")
 
 def test_task_one_style_formatter2():
     """."""
     string = 'file_002 :   123.46, 1.00e+04, 1.23e+04'
     assert task_one_style_formatter2((2, 123.4567, 10000, 12345.67)) == string
-
-    print("Passed test_task_one_style_formatter2")
 
 
 def test_task_three_style_formatter():
@@ -150,14 +160,10 @@ def test_task_three_style_formatter():
     assert task_three_style_formatter((2,3,5)) == res1
     assert task_three_style_formatter((2,3,5,7,9)) == res2
 
-    print("Passed task_three_style_formatter")
-
 
 def test_change_positions():
     """."""
     assert change_positions(( 4, 30, 2017, 2, 27)) == '02 27 2017 04 30'
-
-    print("Passed test_change_positions")
 
 
 def test_f_formatter1():
@@ -165,22 +171,16 @@ def test_f_formatter1():
     output = "The weight of an orange is 1.3 and the weight of a lemon is 1.1"
     assert f_formatter(['oranges', 1.3, 'lemons', 1.1]) == output
 
-    print("Passed test_f_formatter")
-
 
 def test_f_formatter2():
     """."""
     res = "The weight of an ORANGE is 1.56 and the weight of a LEMON is 1.32"
     assert f_formatter(['oranges', 1.3, 'lemons', 1.1]) == res
 
-    print("Passed test_f_formatter")
-
 
 def test_display_col():
     """."""
     raise NotImplementedError
-
-    print("Passed test_display_col")
 
 
 def run_tests():
