447b91c0e52cbc2fb37e4f4077679623cb8aaaa6
Teck1 <Teck1@users.noreply.github.com>
2018-01-28 13:36:19 -0800

    Clean up testing functions. Add remove function.

---
+++ students/ChrisH/lesson03/slicing_lab.py
@@ -0,0 +1,54 @@
+# -----------------------------------------------------------
+# slicing_lab.py
+#  functions that take sequences and return modified copies
+# -----------------------------------------------------------
+
+
+def exchange_first_last(seq):
+    """
+    Take a sequence and return a copy with the first and last items exchanged.
+    :param seq: a sequence of items
+    :return: seq with first/last items exchanged
+    """
+
+    if len(seq) < 2:
+        return seq
+
+    return seq[-1:] + seq[1:-1] + seq[:1]
+
+
+def remove_every_other_item(seq):
+    """
+    Remove every other item in the seq.
+    :param seq: a sequence of items
+    :return: seq with every other item removed
+    """
+    if len(seq) < 2:
+        return seq
+
+    return seq[::2]
+
+
+if __name__ == "__main__":
+    test_set = ['The quick brown fox jumped over the lazy dog.',
+                [0, 1, 2, 3, 4, 5],
+                (0, 1, 2, 3, 4, 5),
+                ]
+
+    exch_set = ['.he quick brown fox jumped over the lazy dogT',
+                [5, 1, 2, 3, 4, 0],
+                (5, 1, 2, 3, 4, 0),
+                ]
+
+    remove_set = ['Teqikbonfxjme vrtelz o.',
+                  [0, 2, 4],
+                  (0, 2, 4)]
+
+    test_functions = [(exchange_first_last, exch_set),
+                      (remove_every_other_item, remove_set)]
+
+    for func, result_set in test_functions:
+        print("Testing {:s}".format(func.__name__))
+        for index, test in enumerate(test_set):
+            print(func(test))
+            assert func(test) == result_set[index]
