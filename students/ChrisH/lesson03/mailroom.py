b11e84ba4ff01a7f1912251acc7d54079a02df22
Teck1 <Teck1@users.noreply.github.com>
2018-01-28 19:04:28 -0800

    Add main procedure.

--- students/ChrisH/lesson03/mailroom.py
+++ students/ChrisH/lesson03/mailroom.py
@@ -2,6 +2,16 @@
 # -----------------------------------------------------------
 # mailroom.py
 #  Part 1. Script to automate writing thank you emails to donors.
+#  Uses only functions and data types learned about so far.
 # -----------------------------------------------------------
 
 
+
+
+
+
+if __name__ == "__main__":
+
+    while(True):
+        # main interaction loop
+        continue
