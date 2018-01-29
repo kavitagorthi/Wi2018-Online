c5dd95f2d1552366d16f13ccc0bef9e2ef70c173
Teck1 <Teck1@users.noreply.github.com>
2018-01-28 18:55:09 -0800

    Create mailroom.py

---
+++ students/ChrisH/lesson03/mailroom.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -----------------------------------------------------------
+# mailroom.py
+#  Part 1. Script to automate writing thank you emails to donors.
+# -----------------------------------------------------------
+
+
