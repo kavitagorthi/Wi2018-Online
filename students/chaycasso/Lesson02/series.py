f9d4dfbe4c33fe87acb4ba61d2878620e4b582bf
Chay Casso <chay@alum.mit.edu>
2018-01-28 19:00:15 -0800

    Initial stab and test of generic sum_series series.

--- students/chaycasso/Lesson02/series.py
+++ students/chaycasso/Lesson02/series.py
@@ -16,7 +16,13 @@ def lucas(n):
     else:
         return lucas(n-1) + lucas(n-2)
 
-print(lucas(8))
-
 def sum_series(n,init1=0,init2=1):
-    """Return the nth value in a generalized sum series defined by init1 and init2."""
+    """Return the nth value in a generalized sum series defined by init1 (n=1) and init2 (n=2)."""
+    if n == 1:
+        return init1
+    elif n == 2:
+        return init2
+    else:
+        return sum_series(n-1,init1,init2) + sum_series(n-2,init1,init2)
+
+print(sum_series(8))
