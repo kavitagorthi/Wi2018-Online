b9403b4281a2b127ec51addd76732fa7a3626d67
Chay Casso <chay@alum.mit.edu>
2018-01-28 18:51:49 -0800

    Initial stab and test of fibonacci fn.

--- students/chaycasso/Lesson02/series.py
+++ students/chaycasso/Lesson02/series.py
@@ -1,6 +1,12 @@
 def fibonacci(n):
-    None
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
+print(fibonacci(8))
 
 def lucas(n):
     None
