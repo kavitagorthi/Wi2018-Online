0d874234be8a211828be34f97726eec805467ee4
Chay Casso <chay@alum.mit.edu>
2018-01-29 19:04:59 -0800

    First attempt at grid part 2.

--- students/chaycasso/Lesson02/gridprinter.py
+++ students/chaycasso/Lesson02/gridprinter.py
@@ -16,4 +16,29 @@ def grid1():
         print(pipe + blank * 9 + pipe + blank * 9 + pipe)
     print(plus + blankminus * 4 + blank + plus + blankminus * 4 + blank + plus)
 
-grid1()
+def GridLine2(gridsize=3):
+    plus = "+"
+    minus = "-"
+    blank = " "
+    pipe = "|"
+    blankminus = blank + minus
+    print(plus + blankminus * ((gridsize - 1) / 2) + blank + plus + blankminus * ((gridsize - 1) / 2) + blank + plus)
+
+def GridSpace2(gridsize=3):
+    plus = "+"
+    minus = "-"
+    blank = " "
+    pipe = "|"
+    blankminus = blank + minus
+    print(pipe + blank * gridsize + pipe + blank * gridsize + pipe)
+
+def grid2(gridsize=3):
+    GridLine2(gridsize)
+    for i in range(1, (gridsize + 1) / 2):
+        GridSpace2(gridsize)
+    GridLine2(gridsize)
+    for i in range(1, (gridsize + 1) / 2):
+        GridSpace2(gridsize)
+    GridLine2(gridsize)
+    
+grid2(3)
