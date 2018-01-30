b7f28a21e9e66dc787c84ca2022eb73919fc23bc
Chay Casso <chay@alum.mit.edu>
2018-01-29 19:35:37 -0800

    Final for problem; some trims to look pretty.

--- students/chaycasso/Lesson02/gridprinter.py
+++ students/chaycasso/Lesson02/gridprinter.py
@@ -16,7 +16,9 @@ def grid1():
         print(pipe + blank * 9 + pipe + blank * 9 + pipe)
     print(plus + blankminus * 4 + blank + plus + blankminus * 4 + blank + plus)
 
+
 def GridLine2(gridsize=3):
+    """Draw a line with dashes and plus signs for a 2x2 grid of total length gridsize."""
     plus = "+"
     minus = "-"
     blank = " "
@@ -24,7 +26,9 @@ def GridLine2(gridsize=3):
     blankminus = blank + minus
     print(plus + blankminus * int((gridsize - 1) / 2) + blank + plus + blankminus * int((gridsize - 1) / 2) + blank + plus)
 
+
 def GridSpace2(gridsize=3):
+    """Draw the inner space of the 2x2 grid with total length gridsize."""
     plus = "+"
     minus = "-"
     blank = " "
@@ -32,7 +36,9 @@ def GridSpace2(gridsize=3):
     blankminus = blank + minus
     print(pipe + blank * gridsize + pipe + blank * gridsize + pipe)
 
+
 def grid2(gridsize=3):
+    """Draw a 2x2 grid of squares with total length gridsize."""
     GridLine2(gridsize)
     for i in range(1, int((gridsize + 1) / 2)):
         GridSpace2(gridsize)
@@ -40,31 +46,34 @@ def grid2(gridsize=3):
     for i in range(1, int((gridsize + 1) / 2)):
         GridSpace2(gridsize)
     GridLine2(gridsize)
+
 
 # The use of gridsize is different between Part 2 and Part 3 because it changes in the given examples for Part 2 and
 # Part 3.
 
 def GridLine3(columns = 2, gridsize = 3):
+    """Draw a line for a grid of columns x columns size, each square a gridsize length."""
     plus = "+"
     minus = "-"
     blank = " "
-    pipe = "|"
     blankminus = blank + minus
     print((plus + blankminus * gridsize + blank) * columns + plus)
 
+
 def GridSpace3(columns = 2, gridsize = 3):
-    plus = "+"
-    minus = "-"
+    """Draw the inner spaces for a grid of columns x columns size, each square a gridsize length."""
     blank = " "
     pipe = "|"
-    blankminus = blank + minus
     print((pipe + blank * (gridsize * 2 + 1)) * columns + pipe)
 
+
 def grid3(columns = 2, gridsize = 3):
-    for j in range(1, columns + 1)
+    """Draw a grid of columns x columns size, each square a gridsize length."""
+    for j in range(1, columns + 1):
         GridLine3(columns, gridsize)
-        for i in range(1, gridsize + 1)):
+        for i in range(1, gridsize + 1):
             GridSpace3(columns, gridsize)
     GridLine3(columns, gridsize)
 
-grid3(3,4)
+
+grid3(5,3)
