68011f9d4bc0e539ee6ff11181b6b17fe3b57b0d
Teck1 <Teck1@users.noreply.github.com>
2018-01-31 16:49:32 -0800

    Add actions for Dictionaries 2 and Sets 1 excercises.

--- students/ChrisH/lesson04/dict_lab.py
+++ students/ChrisH/lesson04/dict_lab.py
@@ -20,8 +20,26 @@ def dictionaries():
     print("Is cake a key?", bool(data.get('cake', 0)))
     print("Is Mango a value?", 'Mango' in data.values())
 
+    # Dictionaries 2
+    new_data = {}
+    for k in data:
+        new_data[k] = (data[k].lower()).count('t')
+    print("New dictionary, same keys, value is count of 't's in the old dictionary value:\n", new_data)
 
 
+def sets():
+
+    # Sets 1
+    s2 = set(range(0, 21, 2))
+    s3 = set(range(0, 21, 3))
+    s4 = set(range(0, 21, 4))
+    print("Set s2:", s2)
+    print("Set s3:", s3)
+    print("Set s4:", s4)
+    print("Is s3 a subset of s2?", s3.issubset(s2))
+    print("Is s4 a subset of s2?", s4.issubset(s2))
+    
 
 if __name__ == "__main__":
-    dictionaries()
+    # dictionaries()
+    sets()
