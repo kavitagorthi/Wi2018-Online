7da9bac109e9b9f1e944676f4d13faea5a90e959
Teck1 <Teck1@users.noreply.github.com>
2018-02-03 15:54:20 -0800

    Add initial trigram generation function.

--- students/ChrisH/lesson04/trigram_kata.py
+++ students/ChrisH/lesson04/trigram_kata.py
@@ -9,8 +9,48 @@
 # word pair as key in the form 'word1 word2'
 # book_trigram_data = { 'word pair': ['list of possible next words']}
 
-def main():
+trigram = {}
 
+
+def generate_trigram(book):
+    word1 = ''
+    word2 = ''
+
+    with open(book, 'r') as f_name:
+        book_text = f_name.read()
+        book_text = book_text.replace('\n', ' ')
+        book_text = book_text.replace('  ', ' \n ')   # See double new lines as word choices
+
+        words = book_text.split(' ')
+
+        for word in words:
+
+            if word1 and word2:
+                newkey = "{} {}".format(word1, word2)
+                if not newkey in trigram:
+                    trigram[newkey] = [word]
+                else:
+                    trigram[newkey].append(word)
+                word1 = word2
+                word2 = word
+
+            elif not word1:
+                word1 = word
+
+            elif not word2:
+                word2 = word
+
+    print(trigram)
+    count = 0
+    for key in trigram:
+        if len(trigram[key]) > 1:
+            print(key, trigram[key])
+            count += 1
+    print(count)
+
+
+def main():
+    generate_trigram('TSAHA.txt')
 
 
 if __name__ == "__main__":
