0e4112ef7b109edb3710c3f3a8371119b05cdcd4
Teck1 <Teck1@users.noreply.github.com>
2018-02-03 14:09:33 -0800

    Add initial program for Kata Fourteen, trigrams. Add sample text files for use with program.

---
+++ students/ChrisH/lesson04/trigram_kata.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -----------------------------------------------------------
+# trigram_kata.py
+#  Implements a trigram algorithm that generates a couple of
+#  hundred words of text using a book-sized file as input
+# -----------------------------------------------------------
+
+
+# word pair as key in the form 'word1 word2'
+# book_trigram_data = { 'word pair': ['list of possible next words']}
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
