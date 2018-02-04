650cf1672d305144bbbab61869d937a39b9beb73
AlexSkrn <askrn123@gmail.com>
2018-02-04 22:55:54 +0300

    Broke down functions into more functions

--- students/alex_skrn/lesson04/a_new_file.py
+++ students/alex_skrn/lesson04/a_new_file.py
@@ -1,48 +1,39 @@
 """Assignment: Kata Fourteen: Tom Swift Under Milk Wood."""
 
-# import os
 import random
 import tkinter as tk
 from tkinter import filedialog
 
-# def tokenize(line):
-#     '''Split string into separate tokens. Return a list.'''
-#     assert isinstance(line, str), 'must be string'
-#     res = ''
-#     for char in line:
-#         if char.isalnum() or char == ' ': ## .isalpha()
-#             res += char
-#         else:
-#             res += ' ' + char + ' '
-#     return res.split()
 
+# load the file containing the book.
 
-# load a file containing a book.
-def load_file():
-    """Return a list containing the whole book split on blank space."""
+def get_filepath():
+    """Return the path to the source file obtained from the user."""
+    # Get the name of the source file from the user.
     root = tk.Tk()
     root.withdraw()
+    path = filedialog.askopenfilename()
+    return path
 
+
+def load_file(source_path):
+    """Return a list containing the whole book split on blank space."""
     # Container for the whole text of the loaded book.
     book = []
-
-    # Get the name of the source file from the user.
-    source_path = filedialog.askopenfilename()
 
     # Convert the whole book into a list.
     with open(source_path, 'r') as fromF:
         for line in fromF:
             if line == "\n" or line == "\r\n":
-                line = line.strip()
+                line = line.strip()  # Get rid of "\r" or "\n" if any
                 line = line.split()
                 line.append("\n\n")  # Take care of paragraphs
             else:
-                line = line.strip()  # Get rid of \r\n if any
+                line = line.strip()
                 line = line.split()
             # line.append("\n")
             book.extend(line)
-            # if len(line) > 2:
-            #     for token in line:
+
     return book
 
 
@@ -62,22 +53,26 @@ def get_dict(a_list):
     return d
 
 
-# Construct the output text.
-def generate_text(source_dict, size=200):
-    """Return a list with the generateed text of the given size."""
-    output = []
-
-    # Choose the first 2 words for the text. Must be an upper case word.
-    for key in source_dict:
-        # print(key)
+def get_seed(a_dict):
+    """Choose the first 2 words for the text."""
+    for key in a_dict:
         try:
-            if key[0].isupper():
+            if key[0].isupper():  # Must be an upper case word
                 seed = key
                 break
         except IndexError:
             pass
+    return seed
+
+
+# Construct the output text.
+def generate_text(source_dict, size):
+    """Return a list with the generateed text of the given size."""
+    output = []
+
+    # Choose the first 2 words for the text. Must be an upper case word.
+    seed = get_seed(source_dict)
     output.extend(seed.split())
-    # print(output)
 
     # Collect all the other words for the final text.
     for _ in range(size):
@@ -114,35 +109,39 @@ def print_onscreen(text):
     print(text)
 
 
-# Write the output text to a file.
-def write_file(text, filename):
-    """Write the output text to a file."""
-    # Get the target directory from the user.
-    target_dir = filedialog.askdirectory()
-    target_path = "{}/{}".format(target_dir, filename)
+def get_destination_path(filename):
+    """Get the destination directory from the user."""
+    destination_dir = filedialog.askdirectory()
+    destination_path = "{}/{}".format(destination_dir, filename)
+    return destination_path
 
+# Write the output text to a file.
+def write_file(text, destination):
+    """Write the output text to a file."""
     # Write the text to the file.
-    with open(target_path, 'w') as toF:
+    with open(destination, 'w') as toF:
         if isinstance(text, str):
             toF.write(text)
         else:
             toF.write(str(text))
 
     # Display the message on screen.
-    msg = "Wrote your text to the file {}\n"
-    print(msg.format(target_path))
+    msg = "Wrote your masterpiece to the file {}\n"
+    print(msg.format(destination))
 
 
 def process_main():
     """Process all operations to generate some trigram-style text."""
-    book = load_file()
-    write_file(book, "book_as_list.txt")
+    path = get_filepath()
+    book = load_file(path)
     source_dict = get_dict(book)
-    write_file(source_dict, "resulting_dict.txt")
-    text_L = generate_text(source_dict)
-    write_file(text_L, "output_as_list.txt")
-    text_S = formatter(text_L)
-    write_file(text_S, "masterpiece.txt")  # Remove str later
+    text_L = generate_text(source_dict, 200)  # Get text as a list
+    text_S = formatter(text_L)  # Convert the list into a string
+    # write_file(book, "book_as_list.txt")
+    # write_file(source_dict, "resulting_dict.txt")
+    # write_file(text_L, "output_as_list.txt")
+    destination = get_destination_path("masterpiece.txt")
+    write_file(text_S, destination)
     print_onscreen(text_S)
 
 
