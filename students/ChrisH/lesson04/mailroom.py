2ab9873e1354730ebdb0549f29a2b3410db37f00
Teck1 <Teck1@users.noreply.github.com>
2018-02-04 17:37:24 -0800

    Changed global donor_data structure to make it a dictionary, in order to simplify usage, and eliminate the possibilty to have duplicate entries. Modified or removed other functions to accomodate data structure change.'

--- students/ChrisH/lesson04/mailroom.py
+++ students/ChrisH/lesson04/mailroom.py
@@ -7,34 +7,14 @@
 
 import time
 
-
 # Global data structure
-donor_data = [{'first_name': 'Al', 'last_name': 'Donor1', 'donations': [10.00, 20.00, 30.00, 40.00, 50.00]},
-              {'first_name': 'Bert', 'last_name': 'Donor2', 'donations': [10.00]},
-              {'first_name': 'Connie', 'last_name': 'Donor3', 'donations': [10.00, 10.00, 10.01]},
-              {'first_name': 'Dennis', 'last_name': 'Donor4', 'donations': [10.00, 20.00, 20.00]},
-              {'first_name': 'Egbert', 'last_name': 'Donor5', 'donations': [10.39, 20.21, 10.59, 4000.40]},
-              ]
-
-
-def donor_fullname(donor):
-    """
-    Given a dictionary entry for a donor, concatenates first and last name into a full name string.
-    :param donor: A donor dictionary object from donor_data
-    :return: string with donor's full name
-    """
-    return donor['first_name'] + ' ' + donor['last_name']
-
-
-def donor_names():
-    """
-    Iterates donor data structure to produce a list of donor names.
-    :return: list of donor names
-    """
-    name_list = []
-    for donor in donor_data:
-        name_list.append(donor_fullname(donor))
-    return name_list
+donor_data = \
+    {'Al Donor1': {'first_name': 'Al', 'last_name': 'Donor1', 'donations': [10.00, 20.00, 30.00, 40.00, 50.00]},
+     'Bert Donor2': {'first_name': 'Bert', 'last_name': 'Donor2', 'donations': [10.00]},
+     'Connie Donor3': {'first_name': 'Connie', 'last_name': 'Donor3', 'donations': [10.00, 10.00, 10.01]},
+     'Dennis Donor4': {'first_name': 'Dennis', 'last_name': 'Donor4', 'donations': [10.00, 20.00, 20.00]},
+     'Egbert Donor5': {'first_name': 'Egbert', 'last_name': 'Donor5', 'donations': [10.39, 20.21, 10.59, 4000.40]},
+     }
 
 
 def menu(menu_data):
@@ -64,28 +44,28 @@ def generate_letter(donor):
     format_string = """
 Dear {first_name} {last_name},
 
-    Thank you for your donation of ${last_donation:.2f}.
+   Thank you for your donation of ${last_donation:.2f}.
 
             Warmest Regards,
                 Local Charity
 """
-    amount = donor['donations'][-1]
-    return format_string.format(last_donation=float(amount), **donor)
+    amount = donor_data[donor]['donations'][-1]
+    return format_string.format(last_donation=float(amount), **donor_data[donor])
+
+#    return "{first_name} {last_name} {donations[2]}".format(**(donor_data[donor]))  # This requires static index!!
 
 
-# noinspection SpellCheckingInspection
 def send_letters_all():
-    # noinspection SpellCheckingInspection
     """
         Runs through donor data structure, generates a thank you letter for each and saves it to
         the current working directory with in a date+donorname.txt file
         :return: None
         """
     for donor in donor_data:
-        print('Generating letter for {first_name} {last_name}'.format(**donor))
+        print(f'Generating letter for {donor}')
         now = time.localtime()
         f_name = f"{now.tm_year}{now.tm_mon:02}{now.tm_mday:02}_"
-        f_name += donor_fullname(donor).replace(" ", "_") + ".txt"
+        f_name += donor.replace(" ", "_") + ".txt"
         file_out = open(f_name, 'w')
         file_out.write(generate_letter(donor))
         file_out.close()
@@ -98,22 +78,20 @@ def send_thank_you():
     and adds it to donor's data. Prints a 'Thank You' email populated with the donor's data.
     :return: None
     """
-    d_list = donor_names()
     while True:
         name = input("Enter a Full Name ('list' to show list of donors): ")
         if name == 'list':
-            print(("{}\n" * len(d_list)).format(*d_list))
+            print(("{}\n" * len(donor_data)).format(*(donor_data.keys())))
             continue
-        if name not in d_list:          # Defines first name as text up to the first space given
+        if name not in donor_data:          # Defines first name as text up to the first space given
             name_sp = name.split(" ")
-            donor_data.append({'first_name': name_sp[0], 'last_name': ' '.join(name_sp[1:]), 'donations': []})
+            donor_data[name] = {'first_name': name_sp[0], 'last_name': ' '.join(name_sp[1:]), 'donations': []}
         break
 
     amount = input("Enter a donation amount for {} : ".format(name))
-    for donor in donor_data:
-        if name == donor_fullname(donor):
-            donor['donations'].append(float(amount))
-            print(generate_letter(donor))
+
+    donor_data[name]['donations'].append(float(amount))
+    print(generate_letter(name))
 
 
 def print_report():
@@ -123,16 +101,15 @@ def print_report():
     """
     # Adjust first column width to accommodate the length of the longest name
     name_max = 26
-    for name in donor_names():
+    for name in donor_data.keys():
         if len(name) > name_max:
             name_max = len(name) + 1
 
     rpt_title = "Donor Name" + ' ' * (name_max - 10) + "| Total Given | Num Gifts | Average Gift"
     print(rpt_title)
     print("-" * len(rpt_title))
-    for donor in donor_data:
-        name = donor_fullname(donor)
-        dons = donor['donations']
+    for name in donor_data.keys():
+        dons = donor_data[name]['donations']
         print(f"{name:{name_max}} $ {sum(dons):>10.2f}   {len(dons):>9}  ${sum(dons)/len(dons):>12.2f}")
 
 
@@ -141,11 +118,11 @@ def nul():
 
 
 if __name__ == "__main__":
-    menu_functions = {0: ['nul', nul],
+    menu_functions = {0: ['nul', nul],                      # could also use 'lambda: None' instead of nul
                       1: ['Send a Thank You', send_thank_you],
                       2: ['Print a report', print_report],
                       3: ['Send letters to everyone', send_letters_all],
                       4: ['Quit', quit],
                       }
     while True:
-        menu(menu_functions)()       # could also use 'lambda: None' instead of nul
+        menu(menu_functions)()
