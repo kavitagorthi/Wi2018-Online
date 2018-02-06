8a9fbcc946430ca77ef6a3862804989ae5bd1b6f
Chay Casso <chay@alum.mit.edu>
2018-02-06 06:40:10 -0800

    Tasks 3 through 5.

--- students/chaycasso/Lesson03/strformat_lab.py
+++ students/chaycasso/Lesson03/strformat_lab.py
@@ -31,8 +31,50 @@ def formatter(in_tuple):
     length = len(in_tuple)
     form_string = f"the {length} numbers are: "
     for i in range(length):
-        form_string.join("{in_tuple[i]:d}:")
+        form_string = "".join([form_string, "{:d}, "])
+    form_string = form_string[:-2]  #Truncating the last comma-space in the string.
     return form_string.format(*in_tuple)
+
 
 print(formatter((2,3,5)))
 print(formatter((2,3,5,7,9)))
+
+# Task Four
+# Given a 5 element tuple:
+# ( 4, 30, 2017, 2, 27)
+# use string formating to print:
+# '02 27 2017 04 30'
+
+initial_tuple = (4, 30, 2017, 2, 27)
+resultant_string3 = "'{:0>2d} {:0>2d} {:0>4d} {:0>2d} {:0>2d}'".format(initial_tuple[3],initial_tuple[4],
+                                                                       initial_tuple[2],initial_tuple[0],
+                                                                       initial_tuple[1])
+print(resultant_string3)
+
+# Task Five
+# Here’s a task for you: Given the following four element list:
+# ['oranges', 1.3, 'lemons', 1.1]
+initial_list = ['oranges', 1.3, 'lemons', 1.1]
+
+# Write an f-string that will display:
+# The weight of an orange is 1.3 and the weight of a lemon is 1.1
+
+resultant_string4 = (f"The weight of an {initial_list[0][:-1]} is {initial_list[1]} and the weight of "
+                     f"a {initial_list[2][:-1]} is {initial_list[3]}.")
+print(resultant_string4)
+
+# Now see if you can change the f-string so that it displays the names of the fruit in upper case, and the weight
+# 20% higher (that is 1.2 times higher).
+
+resultant_string5 = (f"The weight of an {initial_list[0][:-1].upper()} is {initial_list[1] * 1.2} and the weight of "
+                     f"a {initial_list[2][:-1].upper()} is {initial_list[3] * 1.2}.")
+print(resultant_string5)
+
+# Task Six
+# Write some Python code to print a table of several rows, each with a name, an age and a cost. Make sure some of the
+# costs are in the hundreds and thousands to test your alignment specifiers.
+
+# And for an extra task, given a tuple with 10 consecutive numbers, can you work how to quickly print the tuple in
+# columns that are 5 charaters wide? It’s easily done on one short line!
+
+initial_tuple = (1, 10, 100, 1000, 10000, 2, 20, 200, 2000, 20000)
