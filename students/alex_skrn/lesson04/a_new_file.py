7ef8f1539918e9a7f1e719212a7c042f9b36c1b5
AlexSkrn <askrn123@gmail.com>
2018-02-07 22:43:27 +0300

    Added more comments, condensed the code a bit

--- students/alex_skrn/lesson04/a_new_file.py
+++ students/alex_skrn/lesson04/a_new_file.py
@@ -23,10 +23,12 @@ def load_file(source_path):
     # Convert the whole book into a list.
     with open(source_path, 'r') as fromF:
         for line in fromF:
+            # Take care of paragraphs
             if line == "\n" or line == "\r\n":
                 line = line.strip()  # Get rid of "\r" or "\n" if any
                 line = line.split()
-                line.append("\n\n")  # Take care of paragraphs
+                line.append("\n\n")  # Use a double paragraph instead
+            # Punctuation goes with the words to which it relates
             else:
                 line = line.strip()
                 line = line.split()
@@ -35,17 +37,12 @@ def load_file(source_path):
     return book
 
 
-# OUTPUT MANAGEMENT
-def print_onscreen(text):
-    """Print the generated text on screen."""
-    print(text)
-
-
+# OUTPUT FORMAT MANAGEMENT
 def formatter(a_list):
     """Return a string containing the formatted text."""
     result = ""
     for token in a_list:
-        if token == "\n\n":
+        if token == "\n\n":  # Doesn't need a blank space before or after
             result += token
         else:
             result += (token + " ")
@@ -69,35 +66,34 @@ def get_dict(a_list):
     return d
 
 
-def get_seed(a_dict):
-    """Choose _randomly_ the first 2 words for my text."""
-    while True:
-        seed = random.choice(list(a_dict.keys()))
-        # Need an upper case first word and no \n\n in the second position.
-        if seed[0].isupper() and len(seed.split()) == 2:
-            break
-    return seed
-
-
-def get_alt_value(a_dict, key, bad):
+def get_alt_value(a_dict, key, bad_value):
     """Return a random value (if any) but not the bad value."""
-    if len(set(a_dict[key])) == 1:
+    if len(set(a_dict[key])) == 1:  # Exclude values like ["I", "I"]
         return False
     else:
         values = a_dict[key]
-        values.remove(bad)
+        values.remove(bad_value)
         return random.choice(values)
 
 
 def get_next_value(a_dict, key):
     """Return a random value (if any) or False if no such key."""
     try:
-        value = a_dict[key]  # The value here is a list
+        value = a_dict[key]  # The value here is a list, with 1 or more items
     except KeyError:
-        # print("KeyEroor: Dead end occured on the pair {}".format(key))
         return False
 
     return random.choice(value)
+
+
+def get_seed(a_dict):
+    """Choose randomly a key from a_dict as the first 2 words for my text."""
+    while True:
+        seed = random.choice(list(a_dict.keys()))
+        # Want an upper case first word and no \n\n in the second position.
+        if seed[0].isupper() and len(seed.split()) == 2:
+            break
+    return seed
 
 
 def generate_text(source_dict, size):
@@ -130,51 +126,55 @@ def generate_text(source_dict, size):
                     result = result[:counter-1]
                     break
                 counter -= 1
-        # The backtracking was not sucessful
+        # The backtracking was not sucessful, a dead end can't be overcome
         if not value:
             print("Target length cannot be reached")
-            return False
-
+            return result
+        # Everything goes fine so far
         result.append(value)
 
     return result
 
 
-def process_main():
+def process_main(size):
     """Process all operations to generate some trigram-style text."""
     # Get the input book file from the user
     path = get_filepath()
+    # Convert the file into a list of strings
     book = load_file(path)
+    # Generate the dict
     source_dict = get_dict(book)
-    text_L = generate_text(source_dict, 200)  # Get text as a list
-    text_S = formatter(text_L)  # Convert the list into a string
-    print_onscreen(text_S)
+    # Get the generated trigram text as a list
+    text_L = generate_text(source_dict, size)
+    # Format the list into a printable string and print on screen
+    print(formatter(text_L))
 
 
+# TESTING
 def test_generate_text():
-    """Run the generate_text() function multiple times for errors."""
+    """Run  generate_text() multiple times for errors."""
     path = "C:/Users/Alexey/Desktop/sherlock.txt"
     book = load_file(path)
     source_dict = get_dict(book)
-    loop_count = 0
+    run_count = 0
     count_lengths = []
     runs = 100
     size = 1000
     for _ in range(runs):
         res = generate_text(source_dict, size)
         count_lengths.append(len(res))
-        loop_count += 1
-    print("Num of runs = ", loop_count)
-    assert sum(count_lengths) / runs == size, "Mean must be equal to size"
+        run_count += 1
+    print("Num of runs = {}, output size = {}".format(run_count, size))
 
 
 def test_generate_text2():
     """See how it behaves when the target size output cannot be reached."""
     source_d = {"A b": ["c", "d"]}
-    generate_text(source_d, 10)  # Impossible case
+    res = generate_text(source_d, 10)  # Impossible case
+    print(formatter(res))
 
 
 if __name__ == "__main__":
-    process_main()
+    process_main(200)
     # test_generate_text()
     # test_generate_text2()
