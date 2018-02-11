2aeb4019538cbbb411473ecf71e613c9b2ef0457
AlexSkrn <askrn123@gmail.com>
2018-02-11 21:38:00 +0300

    Draft solution

--- students/alex_skrn/lesson05/except_exercise.py
+++ students/alex_skrn/lesson05/except_exercise.py
@@ -14,13 +14,19 @@ from except_test import fun, more_fun, last_fun
 # in that catch block, try again with the second item in the list
 first_try = ['spam', 'cheese', 'mr death']
 
-joke = fun(first_try[0])
+try:
+    joke = fun(first_try[0])
+except NameError:
+    # joke = fun(first_try[1])
+    print("Whoops! there is no joke for: ", first_try[0])
 
 # Here is a try/except block. Add an else that prints not_joke
 try:
     not_joke = fun(first_try[2])
 except SyntaxError:
     print('Run Away!')
+else:
+    print(not_joke)
 
 # What did that do? You can think of else in this context, as well as in
 # loops as meaning: "else if nothing went wrong"
@@ -40,4 +46,10 @@ except SyntaxError:
 
 langs = ['java', 'c', 'python']
 
-more_joke = more_fun(langs[0])
+try:
+    more_joke = more_fun(langs[0])
+except IndexError:
+    next_joke = more_fun(langs[1])
+finally:
+    more_fun(langs[2])
+    last_fun()
