15692c4d4558972e388df1d8d042be37042e1254
ssankura <sireesha@gmail.com>
2018-02-15 09:37:59 +0530

    Changed code to use non-recursive approach to find Fibonacci and Lucas values

--- students/ssankura/lesson02/series.py
+++ students/ssankura/lesson02/series.py
@@ -1,35 +1,65 @@
 """
 Lesson02 - Assignment3 - Sum Series
-contains 3 methods: fibonacci, lucas and sum_series
+NON RECURSIVE approach to compute Fibonacci and Lucas values
+contains 3 methods: fibonacci, lucas and sum_series and global initialization
 code which tests the methods in the "main" method
+This code can compute the Fibonacci or Lucas value for any number from 0 to 10000 
+Need to figure out a way - to dynamically allocate the size of the global lists 
 """
+
+"""
+Using global lists (capacity 10000) and initializing the values to -1
+These lists will store the values of the Fibonacci and Lucas numbers resp.
+If the value is -1, it implies that the value has not been computed for that index
+ex: fib[5] contains the fibonacci number for number 5
+ex: luc[5] contains the Lucas number for number 5
+"""
+fib = []
+n = 100000
+for i in range(0, n+1):
+    fib.append(-1) # you push your element here, as of now I have push i
+
+luc =[]
+for i in range(0,n+1):
+	luc.append(-1)
+
 
 """
 fibonacci method - which returns the fibonacci number for the input number
-starting values - fibonacci(0)=0, fibonacci(1)=1
-any other fibonacci number value is the sum of the previous 2 fibonacci values(numbers)
+starting values - populate the starting values in the list fib[0] and fib[1]
+if the array value is not equal to fib[n] to -1, then return the value
+Else, compute the value using the sum of the previous 2 values in the fib list (which are not -1)
 """
 def fibonacci(n):
-	if n == 0:
-		return 0
-	if n == 1:
-		return 1
-	return (fibonacci(n-1) + fibonacci(n-2))
+	fib[0] = 0
+	fib[1] = 1
+	if fib[n] != -1:
+		return fib[n]
+	#Compute fib[n]
+	for i in range (2,n+1):
+		if fib[i] == -1:
+			fib[i] = fib[i-1] + fib[i-2]
+	return (fib[i])
 
 """
 lucas method - which returns the lucas number for the input number
-starting values - lucas(0) =2, lucas(1)=1
-any other lucas number value is the sum of the previous 2 lucas numbers(values)
+starting values - populate the starting values in the list luc[0] and luc[1]
+if the array value is not equal to luc[n] to -1, then return the value
+Else, compute the value using the sum of the previous 2 values in the luc list (which are not -1)
 """
 def lucas(n):
-	if n == 0:
-		return 2
-	if n == 1:
-		return 1
-	return (lucas(n-1) + lucas(n-2))
-
+	luc[0] = 2
+	luc[1] = 1
+	if luc[n] != -1:
+		return luc[n]
+	#Compute luc[n]
+	for i in range (2,n+1):
+		if luc[i] == -1:
+			luc[i] = luc[i-1] + luc[i-2]
+	return (luc[i])
+	
 """
-sum_series method- with 3 input params 
+sum_series methods- with 3 input params 
 	n - value for which we need to compute fibonacci value or lucas value
 	x - with a default value 0
 	y - with a default value 1
@@ -78,5 +108,10 @@ if __name__ == "__main__":
 	assert (sum_series(20,0,1) == 6765) #fibonacci series value for 20
 	assert(sum_series(20,2,1) == 15127) #lucas series value for 20
 	assert(sum_series(20,1,3) == -1) #not fibonacci or lucas series
+
+	assert (sum_series(100) == 354224848179261915075) #fibonacci series value for 20
+	assert (sum_series(100,0,1) == 354224848179261915075) #fibonacci series value for 20
+	assert(sum_series(100,2,1) == 792070839848372253127) #lucas series value for 20
+	assert(sum_series(100,1,3) == -1) #not fibonacci or lucas series
 
 	print ("Tests passed")
