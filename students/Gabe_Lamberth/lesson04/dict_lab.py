c337547d78d78d55f946d9ea5d22bbc6150d374b
lambocalrissian <gncelambfam@gmail.com>
2018-02-17 16:13:37 -0700

    adding draft dict_lab

---
+++ students/Gabe_Lamberth/lesson04/dict_lab.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Activity 1
+print("{:-^70s}".format("Activity One"), end='\n')
+
+tst_dict = {'name': 'Chris', 'city':'Seattle', 'cake':'Chocolate'}
+print(f'Here are the Keys & Values in the dictonary: \n{tst_dict}')
+
+print("{:-^70s}".format("Activity Two"), end='\n')
+
+
+
