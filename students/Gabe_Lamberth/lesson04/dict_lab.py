581a9bbf193769a389aa22a7ce8958af3e8f5523
lambocalrissian <gncelambfam@gmail.com>
2018-02-17 22:47:41 -0700

    made some line changes

--- students/Gabe_Lamberth/lesson04/dict_lab.py
+++ students/Gabe_Lamberth/lesson04/dict_lab.py
@@ -4,9 +4,13 @@
 print("{:-^70s}".format("Activity One"), end='\n')
 
 tst_dict = {'name': 'Chris', 'city':'Seattle', 'cake':'Chocolate'}
-print(f'Here are the Keys & Values in the dictonary: \n{tst_dict}')
+print(f'Here are the Keys & Values in the dictonary: \n{tst_dict}\n')
+# remove cake entry
+tst_dict.pop("cake")
+
+print(f'Here are the values after removing the "cake" key: \n{tst_dict}\n')
+tst_dict["fruit"] = "Mango"
+print(f'Here are the values after adding "fruit":"Mango "\n {tst_dict}\n')
+print(f'Here are the dictionary "keys": \n{tst_dict.keys()}\n')
 
 print("{:-^70s}".format("Activity Two"), end='\n')
-
-
-
