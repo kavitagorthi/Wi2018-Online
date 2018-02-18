5095e2c69bd01e84cdf3224c3bc4e2b7f8c84d54
Chay Casso <chay@alum.mit.edu>
2018-02-17 20:05:28 -0800

    Some work on converting Thank You to dict.

--- students/chaycasso/Lesson04/mailroom2.py
+++ students/chaycasso/Lesson04/mailroom2.py
@@ -5,44 +5,40 @@
 # 2/11/2018
 
 # Initial donor table with the donation values.
-donor_table = [["William Gates, III", 401321.52], ["William Gates, III", 201342.71], ["Mark Zuckerberg", 123.45],
-               ["Mark Zuckerberg", 5123.21], ["Mark Zuckerberg", 8213.11], ["Jeff Bezos", 877.33],
-               ["Paul Allen", 152.42], ["Paul Allen", 30.54], ["Paul Allen", 825.21], ["Steve Ballmer", 5198.96],
-               ["Steve Ballmer", 654.98]]
+donor_table_dict = {"William Gates, III": [401321.52, 201342.71], "Mark Zuckerberg": [123.45, 5123.21, 8213.11],
+                    "Jeff Bezos": [877.33], "Paul Allen": [152.42, 30.54, 825.21], "Steve Ballmer": [5198.96, 654.98]}
 answer = ""
 
 
-def thank_you(donor_table):
+def thank_you(thank_you_dict):
     full_name = ""
     while True:
         full_name = input("Please enter a full name. >")
-        if full_name.lower() == "quit": return(donor_table)
+        if full_name.lower() == "quit": return(thank_you_dict)
         if full_name.lower() == "list":
-            donor_table.sort()
-            current_name = ""
             print("Donor list:")
-            for row in donor_table:
-                if current_name != row[0]:
-                    current_name = row[0]
-                    print(current_name)
+            print(list(thank_you_dict.keys()))
         else:
             donation_value_str = input("Please enter a donation amount. >")
-            if donation_value_str.lower() == "quit": return(donor_table)
-            donor_table.append([full_name, float(donation_value_str)])
+            if donation_value_str.lower() == "quit": return(thank_you_dict)
+            if full_name in thank_you_dict:
+                thank_you_dict[full_name].append(donation_value_str)
+            else:
+                thank_you_dict[full_name] = donation_value_str
             print()
             print("\nDear {}:\n\n\tThank you for your generous donation of ${} to Save the Kids.\n\n-------------\n"
                   "Save the Kids\nsave@kids.org\n".format(full_name, donation_value_str))
-            return(donor_table)
+            return(thank_you_dict)
 
 
-def create_report(donor_table):
-    donor_table.sort()
+def create_report(create_report_dict):
+
     print("{:<20} | {:<10} | {:<10} | {:<10}".format("Donor Name", "Total Given", "Num Gifts", "Average Gift"))
     print("----------------------------------------------------------------------------")
-    current_name = donor_table[0][0]
+    current_name = create_report_dict[0][0]
     donor_total = 0
     num_donor = 0
-    for row in donor_table:
+    for row in create_report_dict:
         if current_name != row[0]:
             print("{:<20}   ${:>10.2f}   {:>10d}   ${:>10.2f}".format(current_name, donor_total, num_donor,
                                                              donor_total / num_donor))
@@ -58,7 +54,7 @@ def create_report(donor_table):
         print()
 
 
-def send_letters(donor_table):
+def send_letters(donor_table_dict):
     pass
 
 
@@ -66,13 +62,13 @@ def send_letters(donor_table):
 if __name__ == "__main__":
     while True:
         print("1. Send a Thank You\n2. Create a Report\n3. Send Letters to All\n4. Quit")
-        answer_dict = {1: thank_you,
-                       2: create_report,
-                       3: send_letters,
-                       4: "quit"}
+        answer_dict = {"1": thank_you,
+                       "2": create_report,
+                       "3": send_letters,
+                       "4": "quit"}
         answer = input("Please select an option. >")
         if answer == "4":
             print("Have a nice day.")
             break
         else:
-            answer_dict[answer](donor_table)
+            answer_dict[answer](donor_table_dict)
