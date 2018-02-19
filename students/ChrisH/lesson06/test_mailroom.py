6e2184ff84e3073161d613d52f2d985916e30738
Teck1 <Teck1@users.noreply.github.com>
2018-02-19 12:27:36 -0800

    Inital lesson06 testing framework files added.

---
+++ students/ChrisH/lesson06/test_mailroom.py
@@ -0,0 +1,21 @@
+# -----------------------------------------------------------
+# test_mailroom.py
+#  uses unittest module to test mailroom.py program
+#  run with:
+#     $ python -m unittest test_mailroom
+# -----------------------------------------------------------
+
+
+import unittest
+
+class mailroom_test(unittest.TestCase):
+    pass
+
+class mailroom(mailroom_test):
+    def test_1(self):
+        pass
+
+
+
+
+
