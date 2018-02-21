e4b7182a392d01757449c0646bcfa6179d5a98ba
Scott Luse <scottwluse@gmail.com>
2018-02-20 20:30:16 -0800

    ScottL lesson05, mailroom_part3 dict comp update

--- students/ScottL/lesson05/mailroom_part3.py
+++ students/ScottL/lesson05/mailroom_part3.py
@@ -4,9 +4,13 @@
 # Title: Mailroom Part-3 Assignment
 # Dev: Scott Luse
 # Date: Feb 18, 2018
+# ChangeLog: (Who, When, What)
+#   SLuse, 02/20/2018, added try block in process_input_name, line 58
+#   SLuse, 02/20/2018, removed 'return' from functions, not needed
+#   SLuse, 02/20/2018, add f-string with triple quotes to create_individual_letters, line 111
+#   SLuse, 02/20/2018, add dict comp to print_donors, line 95
 #-------------------------------------------------#
 
-from collections import Counter
 
 # -- Data --#
 donor_dict = {
@@ -33,17 +37,17 @@ def GetUserChoice():
 def process_menu(menu_item):
     if menu_item == '1':
         send_thanks()
-        return
+
     elif menu_item == '2':
         format_charity_structure()
-        return
+
     elif menu_item == '3':
         create_individual_letters()
-        return
+
 
 def send_thanks():
     while (True):
-        thank_you_name = input("Please enter full name, enter 'list' for names, or 'main' for menu: ")
+        thank_you_name = input("Please enter FULL NAME, enter 'list' for names, or 'main' for menu: ")
         if thank_you_name.lower() == "list":
             print_donors()
             print(' ')
@@ -51,28 +55,29 @@ def send_thanks():
             return
         else:
             process_input_name(thank_you_name)
-            return
+
 
 def process_input_name (name):
     '''Ask for a donation amount and look for user input name in donor dictionary.
     If you find the name and the amount to the donor ledger and print a letter to the screen.
     If you don't find the name create a new key value pair'''
+    try:
 
-    gift_amount = int(input("Please enter $$ donation $$ amount for " + name + ":"))
-    # add ValueError: try block
-    if name in donor_dict.keys():
-        #We found the name so update the giving values
-        #Is this the best way to manage appending the list?
-        original_list = donor_dict.get(name)
-        original_list.append(gift_amount)
-        donor_dict[name] = original_list
-        thank_you_printing(name, gift_amount)
-        return
-    else:
-        #We didn't find the name so create a new key value pair
-        donor_dict[name] = [gift_amount]
-        thank_you_printing(name, gift_amount)
-        return
+        gift_amount = int(input("Please enter $$ donation $$ amount for " + name + ":"))
+        if name in donor_dict.keys():
+            #We found the name so update the giving values
+            original_list = donor_dict.get(name)
+            original_list.append(gift_amount)
+            donor_dict[name] = original_list
+            thank_you_printing(name, gift_amount)
+
+        else:
+            #We didn't find the name so create a new key value pair
+            donor_dict[name] = [gift_amount]
+            thank_you_printing(name, gift_amount)
+    except ValueError as e:
+        print("\n Error: " + str(e) + " Please enter a number amount.\n ")
+        process_input_name(name)
 
 def thank_you_printing(name, amount):
     line_divider = "*" * 50
@@ -87,10 +92,8 @@ def thank_you_printing(name, amount):
     ''')
 
 def print_donors():
-    # dict comp opportunity! {print(key) for key in DONOR_HISTORY_DICT.keys())}
-    # PR comment from Michael, update code
-    for key in donor_dict:
-        print(key)
+    comp = {key for key in donor_dict.keys()}
+    print(comp)
 
 def format_charity_structure():
     print('')
@@ -108,13 +111,9 @@ def create_individual_letters():
         for key in donor_dict:
             donor_list = donor_dict.get(key)
             sum_values = "{:.2f}".format(sum(donor_list))
-
-            # Update with triple quotes per Pull Request comments
-
-            report_text = f"Dear {key}," + "\n" + "\n" + "Thank you for your charitable gift of $" + sum_values + "."
-            report_text = report_text  + "\n" + "\n" + "\tIt will be put to very good use."
-            report_text = report_text  + "\n" + "\n" + "\t\tSincerely,"
-            report_text = report_text + "\n" + "\t\t\t--The Team"
+            report_text = (f'''Dear {key},\n\nThank you for your charitable gift of ${sum_values}.
+            \n\tIt will be put to very good use.
+            \n\n\t\tSincerely, \n\t\t\t--The Cool Team''')
 
             key.replace(" ", "_")
             objFileName = key.replace(" ", "_") + ".txt"
