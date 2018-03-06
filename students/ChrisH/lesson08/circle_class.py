085be64508730bafd6b27c3f12dd94910ffd18ae
Teck1 <Teck1@users.noreply.github.com>
2018-03-05 21:05:29 -0800

    Lesson08 circle class, steps 1-6.

--- students/ChrisH/lesson08/circle_class.py
+++ students/ChrisH/lesson08/circle_class.py
@@ -2,11 +2,32 @@
 # -----------------------------------------------------------
 #  Implements a class that represents a single circle.
 # -----------------------------------------------------------
-
+import math
 
 class Circle(object):
 
-    def __init__(self, radius=1):
+    def __init__(self, radius=1.0):
         self.radius = radius
 
+    @property
+    def diameter(self):
+        return self.radius * 2.0
 
+    @diameter.setter
+    def diameter(self, val):
+        self.radius = val / 2.0
+
+    @property
+    def area(self):
+        return math.pi * self.radius**2
+
+    @classmethod
+    def from_diameter(cls, diameter):
+        radius = diameter / 2.0
+        return cls(radius)
+
+    def __str__(self):
+        return f"Circle with radius: {self.radius}"
+
+    def __repr__(self):
+        return f"Circle({self.radius})"
--- students/ChrisH/lesson08/test_circle_class.py
+++ students/ChrisH/lesson08/test_circle_class.py
@@ -7,15 +7,51 @@
 
 import unittest
 from io import StringIO
-# from contextlib import redirect_stdout
-import circle_class as cc
+from contextlib import redirect_stdout
+from circle_class import Circle
 
 class CircleClassTest(unittest.TestCase):
 
     def test_circle_init(self):
-        c = cc.Circle()
+        c = Circle()
         self.assertEqual(c.radius, 1)
-        c = cc.Circle(4)
+        c = Circle(4)
         self.assertEqual(c.radius, 4)
+
+    def test_diameter(self):
+        c = Circle()
+        self.assertEqual(c.diameter, 2)
+        c = Circle(4)
+        self.assertEqual(c.diameter, 8)
+
+    def test_set_diameter(self):
+        c = Circle(4)
+        c.diameter = 2
+        self.assertEqual(c.diameter, 2)
+        self.assertEqual(c.radius, 1)
+
+    def test_get_area(self):
+        c = Circle(2)
+        self.assertAlmostEqual(c.area, 12.5663706, places=7)
+        with self.assertRaises(AttributeError):
+            c.area = 13
+
+    def test_from_diameter(self):
+        c = Circle.from_diameter(8)
+        self.assertEqual(c.diameter, 8)
+        self.assertEqual(c.radius, 4)
+
+    def test_str_method(self):
+        c = Circle()
+        f = StringIO()
+        with redirect_stdout(f):
+            print(c)
+        self.assertIn("Circle with radius: 1.", f.getvalue())
+
+    def test_repr_method(self):
+        c = Circle()
+        self.assertEqual("Circle(1.0)", repr(c))
+        d = eval(repr(c))
+        self.assertEqual(c.radius, d.radius)
 
 
